6723827ce4b68937f15172d64dca128dd4334635
praveenkumarct <praveen.kumar@commercetools.com>
2024-01-19 09:52:21 +0100

    Remove processor skeleton, it should use existing from checkout code

--- processor/routes/payments.route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-import { paymentHandler } from "../controller/payment.controller.js";
-
-const taxCalculatorRouter = Router();
-
-taxCalculatorRouter.post('/payment-methods', paymentHandler);
-
-export default taxCalculatorRouter;
--- processor/utils/logger.utils.js
+++
@@ -1,3 +0,0 @@
-import { createApplicationLogger } from '@commercetools-backend/loggers';
-
-export const logger = createApplicationLogger();

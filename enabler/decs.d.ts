2c8000a4e119ab06e3f291447e68846d944d0ab9
praveenkumarct <praveen.kumar@commercetools.com>
2024-02-02 14:32:11 +0100

    Remove Enabler temporarily for testing Processor

--- enabler/decs.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "@adyen/adyen-web" 
-declare module "@adyen/adyen-web/dist/types/components/ApplePay" 
-declare module "@adyen/adyen-web/dist/types/components/GooglePay" 
-declare module "@adyen/adyen-web/dist/types/core" 
-declare module "@adyen/adyen-web/dist/types/core/core" 
-declare module "@adyen/adyen-web/dist/types/components/Redirect/Redirect" 
-declare module "@adyen/adyen-web/dist/types/core/types"
--- enabler/src/main.ts
+++
@@ -1,4 +0,0 @@
-import '@adyen/adyen-web/dist/adyen.css';
-import { AdyenPaymentConnector } from './payment-connector/adyenPaymentConnector';
-
-export { AdyenPaymentConnector as Connector };

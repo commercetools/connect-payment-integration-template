62b7e035fddbd7b2cafc19c2dcd2eef3356a889b
King-Hin Leung <kinghin.leung@commercetools.com>
2024-02-20 14:39:10 +0100

    Remove skipped unit test

--- processor/test/operation.service.spec.ts
+++ processor/test/operation.service.spec.ts
@@ -6,19 +6,12 @@ import {
   ModifyPayment,
 } from '../src/services/types/operation.type';
 import { DefaultOperationService } from '../src/services/operation.service';
-import { StatusResponseSchemaDTO } from '../src/dtos/operations/status.dto';
-
 import { MockOperationProcessor } from '../src/services/processors/mock-operation.processor';
 import { paymentSDK } from '../src/payment-sdk';
-import sinon from 'sinon';
-import * as connectpaymentsSDK from '@commercetools/connect-payments-sdk';
-import { CommercetoolsAuthorizationService } from '@commercetools/connect-payments-sdk/dist/commercetools';
-
-import { HealthCheckResult } from '@commercetools/connect-payments-sdk/dist/api/handlers/status.handler';
-import * as Config from '../src/config/config';
-
 import { DefaultPaymentService } from '@commercetools/connect-payments-sdk/dist/commercetools/services/ct-payment.service';
 import { mockGetPaymentResult, mockUpdatePaymentResult } from './utils/mock-payment-data';
+import sinon from 'sinon';
+import * as Config from '../src/config/config';
 
 describe('operation.service', () => {
   const opts: OperationServiceOptions = {
@@ -72,32 +65,5 @@ describe('operation.service', () => {
     });
     const result = await opService.modifyPayment(modifyPaymentOpts);
     expect(result?.outcome).toStrictEqual('approved');
-  });
-
-  test.skip('getStatus', async () => {
-    const getMockFunction: () => Promise<HealthCheckResult> = async () => {
-      return {
-        name: 'CoCo Permissions',
-        status: 'UP',
-      };
-    };
-
-    const fakeCall: (opts: {
-      ctAuthorizationService: CommercetoolsAuthorizationService;
-      projectKey: string;
-      requiredPermissions: string[];
-    }) => () => Promise<HealthCheckResult> = () => {
-      return getMockFunction;
-    };
-
-    sinon.stub(connectpaymentsSDK, 'healthCheckCommercetoolsPermissions').callsFake(fakeCall);
-    const opService: OperationService = new DefaultOperationService(opts);
-    const result: StatusResponseSchemaDTO = await opService.getStatus();
-    expect(result?.status).toStrictEqual('OK');
-    expect(result?.checks).toHaveLength(2);
-    expect(result?.checks[0]?.name).toStrictEqual('CoCo Permissions');
-    expect(result?.checks[0]?.status).toStrictEqual('UP');
-    expect(result?.checks[1]?.name).toStrictEqual('Mock Payment API');
-    expect(result?.checks[1]?.status).toStrictEqual('UP');
   });
 });

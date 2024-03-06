cee2afe3f8975c86f0eba9e9bbf0c449a07f747e
King-Hin Leung <35692276+leungkinghin@users.noreply.github.com>
2024-03-06 12:14:30 +0100

    Update mock-payment.service.spec.ts (#85)

--- processor/test/mock-payment.service.spec.ts
+++ processor/test/mock-payment.service.spec.ts
@@ -1,6 +1,5 @@
 import { describe, test, expect, afterEach, jest, beforeEach } from '@jest/globals';
-import { ConfigResponse, ModifyPayment } from '../src/services/types/operation.type';
-
+import { ConfigResponse, ModifyPayment, StatusResponse } from '../src/services/types/operation.type';
 import { paymentSDK } from '../src/payment-sdk';
 import { DefaultPaymentService } from '@commercetools/connect-payments-sdk/dist/commercetools/services/ct-payment.service';
 import { mockGetPaymentResult, mockUpdatePaymentResult } from './utils/mock-payment-data';
@@ -8,6 +7,9 @@ import * as Config from '../src/config/config';
 import { MockPaymentServiceOptions } from '../src/services/types/mock-payment.type';
 import { AbstractPaymentService } from '../src/services/abstract-payment.service';
 import { MockPaymentService } from '../src/services/mock-payment.service';
+import * as StatusHandler from '@commercetools/connect-payments-sdk/dist/api/handlers/status.handler';
+
+import { HealthCheckResult } from '@commercetools/connect-payments-sdk';
 
 interface FlexibleConfig {
   [key: string]: string; // Adjust the type according to your config values
@@ -53,6 +55,31 @@ describe('mock-payment.service', () => {
     const result: ConfigResponse = await paymentService.getSupportedPaymentComponents();
     expect(result?.components).toHaveLength(1);
     expect(result?.components[0]?.type).toStrictEqual('card');
+  });
+
+  test('getStatus', async () => {
+    const mockHealthCheckFunction: () => Promise<HealthCheckResult> = async () => {
+      const result: HealthCheckResult = {
+        name: 'CoCo Permissions',
+        status: 'DOWN',
+        details: {},
+      };
+      return result;
+    };
+
+    jest.spyOn(StatusHandler, 'healthCheckCommercetoolsPermissions').mockReturnValue(mockHealthCheckFunction);
+    const paymentService: AbstractPaymentService = new MockPaymentService(opts);
+    const result: StatusResponse = await paymentService.status();
+
+    expect(result?.status).toBeDefined();
+    expect(result?.checks).toHaveLength(2);
+    expect(result?.status).toStrictEqual('Partially Available');
+    expect(result?.checks[0]?.name).toStrictEqual('CoCo Permissions');
+    expect(result?.checks[0]?.status).toStrictEqual('DOWN');
+    expect(result?.checks[0]?.details).toStrictEqual({});
+    expect(result?.checks[1]?.name).toStrictEqual('Mock Payment API');
+    expect(result?.checks[1]?.status).toStrictEqual('UP');
+    expect(result?.checks[1]?.details).toBeDefined();
   });
 
   test('modifyPayment', async () => {

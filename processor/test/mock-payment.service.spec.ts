4b1141941029d420e703cede68fd854189de10b8
King-Hin Leung <kinghin.leung@commercetools.com>
2024-03-19 17:56:13 +0100

    Fix payment result mocking

--- processor/test/mock-payment.service.spec.ts
+++ processor/test/mock-payment.service.spec.ts
@@ -2,14 +2,18 @@ import { describe, test, expect, afterEach, jest, beforeEach } from '@jest/globa
 import { ConfigResponse, ModifyPayment, StatusResponse } from '../src/services/types/operation.type';
 import { paymentSDK } from '../src/payment-sdk';
 import { DefaultPaymentService } from '@commercetools/connect-payments-sdk/dist/commercetools/services/ct-payment.service';
-import { mockGetPaymentResult, mockUpdatePaymentResult } from './utils/mock-payment-data';
+import { DefaultCartService } from '@commercetools/connect-payments-sdk/dist/commercetools/services/ct-cart.service';
+import { mockGetPaymentResult, mockUpdatePaymentResult } from './utils/mock-payment-results';
+import { mockGetCartResult } from './utils/mock-cart-data';
 import * as Config from '../src/config/config';
-import { MockPaymentServiceOptions } from '../src/services/types/mock-payment.type';
+import { CreatePayment, MockPaymentServiceOptions } from '../src/services/types/mock-payment.type';
 import { AbstractPaymentService } from '../src/services/abstract-payment.service';
 import { MockPaymentService } from '../src/services/mock-payment.service';
+import * as FastifyContext from '../src/libs/fastify/context/context';
 import * as StatusHandler from '@commercetools/connect-payments-sdk/dist/api/handlers/status.handler';
 
 import { HealthCheckResult } from '@commercetools/connect-payments-sdk';
+import { PaymentOutcome } from '../src/dtos/mock-payment.dto';
 
 interface FlexibleConfig {
   [key: string]: string; // Adjust the type according to your config values
@@ -30,7 +34,7 @@ describe('mock-payment.service', () => {
     ctPaymentService: paymentSDK.ctPaymentService,
   };
   const paymentService: AbstractPaymentService = new MockPaymentService(opts);
-
+  const mockPaymentService: MockPaymentService = new MockPaymentService(opts);
   beforeEach(() => {
     jest.setTimeout(10000);
     jest.resetAllMocks();
@@ -94,8 +98,10 @@ describe('mock-payment.service', () => {
       },
     };
 
-    jest.spyOn(DefaultPaymentService.prototype, 'getPayment').mockResolvedValue(mockGetPaymentResult);
-    jest.spyOn(DefaultPaymentService.prototype, 'updatePayment').mockResolvedValue(mockUpdatePaymentResult);
+    jest.spyOn(DefaultPaymentService.prototype, 'getPayment').mockReturnValue(Promise.resolve(mockGetPaymentResult));
+    jest
+      .spyOn(DefaultPaymentService.prototype, 'updatePayment')
+      .mockReturnValue(Promise.resolve(mockUpdatePaymentResult));
 
     const result = await paymentService.modifyPayment(modifyPaymentOpts);
     expect(result?.outcome).toStrictEqual('approved');
@@ -117,9 +123,13 @@ describe('mock-payment.service', () => {
       },
     };
 
-    jest.spyOn(DefaultPaymentService.prototype, 'getPayment').mockResolvedValue(mockGetPaymentResult);
-    jest.spyOn(DefaultPaymentService.prototype, 'updatePayment').mockResolvedValue(mockUpdatePaymentResult);
-    jest.spyOn(DefaultPaymentService.prototype, 'updatePayment').mockResolvedValue(mockUpdatePaymentResult);
+    jest.spyOn(DefaultPaymentService.prototype, 'getPayment').mockReturnValue(Promise.resolve(mockGetPaymentResult));
+    jest
+      .spyOn(DefaultPaymentService.prototype, 'updatePayment')
+      .mockReturnValue(Promise.resolve(mockUpdatePaymentResult));
+    jest
+      .spyOn(DefaultPaymentService.prototype, 'updatePayment')
+      .mockReturnValue(Promise.resolve(mockUpdatePaymentResult));
 
     const result = await paymentService.modifyPayment(modifyPaymentOpts);
     expect(result?.outcome).toStrictEqual('approved');
@@ -141,11 +151,39 @@ describe('mock-payment.service', () => {
       },
     };
 
-    jest.spyOn(DefaultPaymentService.prototype, 'getPayment').mockResolvedValue(mockGetPaymentResult);
-    jest.spyOn(DefaultPaymentService.prototype, 'updatePayment').mockResolvedValue(mockUpdatePaymentResult);
-    jest.spyOn(DefaultPaymentService.prototype, 'updatePayment').mockResolvedValue(mockUpdatePaymentResult);
+    jest.spyOn(DefaultPaymentService.prototype, 'getPayment').mockReturnValue(Promise.resolve(mockGetPaymentResult));
+    jest
+      .spyOn(DefaultPaymentService.prototype, 'updatePayment')
+      .mockReturnValue(Promise.resolve(mockUpdatePaymentResult));
+    jest
+      .spyOn(DefaultPaymentService.prototype, 'updatePayment')
+      .mockReturnValue(Promise.resolve(mockUpdatePaymentResult));
 
     const result = await paymentService.modifyPayment(modifyPaymentOpts);
     expect(result?.outcome).toStrictEqual('approved');
+  });
+
+  test('createCardPayPayment', async () => {
+    const createPaymentOpts: CreatePayment = {
+      data: {
+        paymentMethod: {
+          type: 'card',
+          cardNumber: '1234-1234-1234-1234',
+          expiryMonth: 12,
+          expiryYear: 2046,
+          cvc: 130,
+          holderName: 'Christopher',
+        },
+      },
+    };
+    jest.spyOn(DefaultCartService.prototype, 'getCart').mockReturnValue(Promise.resolve(mockGetCartResult()));
+    jest.spyOn(DefaultPaymentService.prototype, 'createPayment').mockReturnValue(Promise.resolve(mockGetPaymentResult));
+    jest.spyOn(DefaultCartService.prototype, 'addPayment').mockReturnValue(Promise.resolve(mockGetCartResult()));
+    jest.spyOn(FastifyContext, 'getProcessorUrlFromContext').mockReturnValue('http://127.0.0.1');
+    jest.spyOn(DefaultPaymentService.prototype, 'updatePayment').mockReturnValue(Promise.resolve(mockGetPaymentResult));
+
+    const result = await mockPaymentService.createPayment(createPaymentOpts);
+    expect(result?.outcome).toStrictEqual(PaymentOutcome.REJECTED);
+    expect(result?.paymentReference).toStrictEqual('123456');
   });
 });

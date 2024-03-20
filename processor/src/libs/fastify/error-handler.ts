d99d103f01acdd852c782428449e58cac9f9d0d4
King-Hin Leung <kinghin.leung@commercetools.com>
2024-03-20 15:50:57 +0100

    Enhance error-handler unit test

--- processor/src/libs/fastify/error-handler.ts
+++ processor/src/libs/fastify/error-handler.ts
@@ -1,10 +1,117 @@
-import { type FastifyReply, type FastifyRequest } from 'fastify';
+import { FastifyError, type FastifyReply, type FastifyRequest } from 'fastify';
 
-import { ErrorInvalidField, ErrorRequiredField, Errorx, MultiErrorx } from '@commercetools/connect-payments-sdk';
+import { FastifySchemaValidationError } from 'fastify/types/schema';
 import { log } from '../logger';
+import {
+  ErrorAuthErrorResponse,
+  ErrorGeneral,
+  ErrorInvalidField,
+  ErrorInvalidJsonInput,
+  ErrorRequiredField,
+  Errorx,
+  MultiErrorx,
+} from '@commercetools/connect-payments-sdk';
+import { TAuthErrorResponse, TErrorObject, TErrorResponse } from './dtos/error.dto';
+
+function isFastifyValidationError(error: Error): error is FastifyError {
+  return (error as unknown as FastifyError).validation != undefined;
+}
+
+export const errorHandler = (error: Error, req: FastifyRequest, reply: FastifyReply) => {
+  if (isFastifyValidationError(error) && error.validation) {
+    return handleErrors(transformValidationErrors(error.validation, req), reply);
+  } else if (error instanceof ErrorAuthErrorResponse) {
+    return handleAuthError(error, reply);
+  } else if (error instanceof Errorx) {
+    return handleErrors([error], reply);
+  } else if (error instanceof MultiErrorx) {
+    return handleErrors(error.errors, reply);
+  }
+
+  // If it isn't any of the cases above (for example a normal Error is thrown) then fallback to a general 500 internal server error
+  return handleErrors([new ErrorGeneral('Internal server error.', { cause: error, skipLog: false })], reply);
+};
+
+const handleAuthError = (error: ErrorAuthErrorResponse, reply: FastifyReply) => {
+  const transformedErrors: TErrorObject[] = transformErrorxToHTTPModel([error]);
+
+  const response: TAuthErrorResponse = {
+    message: error.message,
+    statusCode: error.httpErrorStatus,
+    errors: transformedErrors,
+    error: transformedErrors[0].code,
+    error_description: transformedErrors[0].message,
+  };
+
+  return reply.code(error.httpErrorStatus).send(response);
+};
+
+const handleErrors = (errorxList: Errorx[], reply: FastifyReply) => {
+  const transformedErrors: TErrorObject[] = transformErrorxToHTTPModel(errorxList);
+
+  // Based on CoCo specs, the root level message attribute is always set to the values from the first error. MultiErrorx enforces the same HTTP status code.
+  const response: TErrorResponse = {
+    message: errorxList[0].message,
+    statusCode: errorxList[0].httpErrorStatus,
+    errors: transformedErrors,
+  };
+
+  return reply.code(errorxList[0].httpErrorStatus).send(response);
+};
+
+const transformErrorxToHTTPModel = (errors: Errorx[]): TErrorObject[] => {
+  const errorObjectList: TErrorObject[] = [];
+
+  for (const err of errors) {
+    if (err.skipLog) {
+      log.debug(err.message, err);
+    } else {
+      log.error(err.message, err);
+    }
+
+    const tErrObj: TErrorObject = {
+      code: err.code,
+      message: err.message,
+      ...(err.fields ? err.fields : {}), // Add any additional field to the response object (which will differ per type of error)
+    };
+
+    errorObjectList.push(tErrObj);
+  }
+
+  return errorObjectList;
+};
+
+const transformValidationErrors = (errors: FastifySchemaValidationError[], req: FastifyRequest): Errorx[] => {
+  const errorxList: Errorx[] = [];
+
+  for (const err of errors) {
+    switch (err.keyword) {
+      case 'required':
+        errorxList.push(new ErrorRequiredField(err.params.missingProperty as string));
+        break;
+      case 'enum':
+        errorxList.push(
+          new ErrorInvalidField(
+            getKeys(err.instancePath).join('.'),
+            getPropertyFromPath(err.instancePath, req.body),
+            err.params.allowedValues as string,
+          ),
+        );
+        break;
+    }
+  }
+
+  // If we cannot map the validation error to a CoCo error then return a general InvalidJsonError
+  if (errorxList.length === 0) {
+    errorxList.push(new ErrorInvalidJsonInput());
+  }
+
+  return errorxList;
+};
 
 const getKeys = (path: string) => path.replace(/^\//, '').split('/');
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const getPropertyFromPath = (path: string, obj: any): any => {
   const keys = getKeys(path);
   let value = obj;
@@ -12,81 +119,4 @@ const getPropertyFromPath = (path: string, obj: any): any => {
     value = value[key];
   }
   return value;
-};
-
-type ValidationObject = {
-  validation: object;
-};
-
-type TError = {
-  statusCode: number;
-  code: string;
-  message: string;
-  errors?: object[];
-};
-
-export const errorHandler = (error: Error, req: FastifyRequest, reply: FastifyReply) => {
-  if (error instanceof Object && (error as unknown as ValidationObject).validation) {
-    const errorsList: Errorx[] = [];
-
-    // Transforming the validation errors
-    for (const err of (error as any).validation) {
-      switch (err.keyword) {
-        case 'required':
-          errorsList.push(new ErrorRequiredField(err.params.missingProperty));
-          break;
-        case 'enum':
-          errorsList.push(
-            new ErrorInvalidField(
-              getKeys(err.instancePath).join('.'),
-              getPropertyFromPath(err.instancePath, req.body),
-              err.params.allowedValues,
-            ),
-          );
-      }
-
-      if (errorsList.length > 1) {
-        error = new MultiErrorx(errorsList);
-      } else {
-        error = errorsList[0];
-      }
-    }
-  }
-
-  if (error instanceof Errorx) {
-    return handleErrorx(error, reply);
-  } else {
-    const { message, ...meta } = error;
-    log.error(message, meta);
-    return reply.code(500).send({
-      code: 'General',
-      message: 'Internal server error.',
-      statusCode: 500,
-    });
-  }
-};
-
-const handleErrorx = (error: Errorx, reply: FastifyReply) => {
-  const { message, ...meta } = error;
-  log.error(message, meta);
-  const errorBuilder: TError = {
-    statusCode: error.httpErrorStatus,
-    code: error.code,
-    message: error.message,
-  };
-
-  const errors: object[] = [];
-  if (error.fields) {
-    errors.push({
-      code: error.code,
-      message: error.message,
-      ...error.fields,
-    });
-  }
-
-  if (errors.length > 0) {
-    errorBuilder.errors = errors;
-  }
-
-  return reply.code(error.httpErrorStatus).send(errorBuilder);
 };
--- processor/test/libs/fastify/error-handler.spec.ts
+++ processor/test/libs/fastify/error-handler.spec.ts
@@ -1,8 +1,9 @@
 import { afterEach, beforeEach, describe, expect, test } from '@jest/globals';
-import Fastify, { type FastifyInstance } from 'fastify';
+import Fastify, { type FastifyInstance, FastifyError } from 'fastify';
 import { errorHandler } from '../../../src/libs/fastify/error-handler';
-import { Errorx } from '@commercetools/connect-payments-sdk';
+import { ErrorAuthErrorResponse, Errorx, ErrorxAdditionalOpts } from '@commercetools/connect-payments-sdk';
 import { requestContextPlugin } from '../../../src/libs/fastify/context/context';
+import { FastifySchemaValidationError } from 'fastify/types/schema';
 
 describe('error-handler', () => {
   let fastify: FastifyInstance;
@@ -31,9 +32,14 @@ describe('error-handler', () => {
     });
 
     expect(response.json()).toStrictEqual({
-      code: 'ErrorCode',
       message: 'someMessage',
       statusCode: 404,
+      errors: [
+        {
+          code: 'ErrorCode',
+          message: 'someMessage',
+        },
+      ],
     });
   });
 
@@ -55,7 +61,6 @@ describe('error-handler', () => {
     });
 
     expect(response.json()).toStrictEqual({
-      code: 'ErrorCode',
       message: 'someMessage',
       statusCode: 404,
       errors: [
@@ -79,9 +84,83 @@ describe('error-handler', () => {
     });
 
     expect(response.json()).toStrictEqual({
-      code: 'General',
       message: 'Internal server error.',
       statusCode: 500,
+      errors: [
+        {
+          code: 'General',
+          message: 'Internal server error.',
+        },
+      ],
+    });
+  });
+
+  test('Fastify error with missing required field', async () => {
+    const validationError: FastifySchemaValidationError = {
+      keyword: 'required',
+      instancePath: 'instancePath/domain/value',
+      schemaPath: 'schemaPath/domain/value',
+      params: {
+        missingProperty: 'dummy-field',
+      },
+      message: 'fastify-error-message',
+    };
+    const fastifyError: FastifyError = {
+      code: 'ErrorCode',
+      name: 'fastify-error',
+      message: 'fastify-error-message',
+      validation: [validationError],
+    };
+    fastify.get('/', () => {
+      throw fastifyError;
+    });
+
+    const response = await fastify.inject({
+      method: 'GET',
+      url: '/',
+    });
+    expect(response.json()).toStrictEqual({
+      message: 'A value is required for field dummy-field.',
+      statusCode: 400,
+      errors: [
+        {
+          code: 'RequiredField',
+          field: 'dummy-field',
+          message: 'A value is required for field dummy-field.',
+        },
+      ],
+    });
+  });
+
+  test('ErrorAuthErrorResponse', async () => {
+    const opts: ErrorxAdditionalOpts = {
+      privateFields: {},
+      privateMessage: '',
+      fields: {},
+      skipLog: true,
+      cause: undefined,
+    };
+    const authErrorResponse: ErrorAuthErrorResponse = new ErrorAuthErrorResponse('someMessage', opts, '401');
+
+    fastify.get('/', () => {
+      throw authErrorResponse;
+    });
+
+    const response = await fastify.inject({
+      method: 'GET',
+      url: '/',
+    });
+    expect(response.json()).toStrictEqual({
+      message: 'someMessage',
+      statusCode: 401,
+      errors: [
+        {
+          code: '401',
+          message: 'someMessage',
+        },
+      ],
+      error: '401',
+      error_description: 'someMessage',
     });
   });
 });

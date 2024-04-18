0eb92a27f5526aef85490c5d3ad9aa94403b680c
Danilo Delizia <104627863+ddeliziact@users.noreply.github.com>
2024-04-18 12:27:39 +0200

    doc(enabler): adding jsdoc to the enabler types (#179)

--- enabler/src/fake-sdk.ts
+++ enabler/src/fake-sdk.ts
@@ -1,9 +1,23 @@
+
+/**
+ * Represents a fake SDK.
+ */
 export class FakeSdk {
   private environment: string;
+
+  /**
+   * Creates an instance of FakeSdk.
+   * @param environment - The environment for the SDK.
+   */
   constructor({ environment }) {
     this.environment = environment;
     console.log('FakeSdk constructor', this.environment);
   }
+
+  /**
+   * Initializes the SDK with the specified options.
+   * @param opts - The options for initializing the SDK.
+   */
   init(opts: any) {
     console.log('FakeSdk init', opts);
   }

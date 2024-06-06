00d622ea3a369ae9ce7778467c96c64d5a504c69
Danilo Delizia <danilo.delizia@commercetools.com>
2024-06-06 11:44:31 +0200

    fix(payment-sdk): status handler

--- processor/src/services/mock-payment.service.ts
+++ processor/src/services/mock-payment.service.ts
@@ -14,7 +14,7 @@ import packageJSON from '../../package.json';
 
 import { AbstractPaymentService } from './abstract-payment.service';
 import { getConfig } from '../config/config';
-import { paymentSDK } from '../payment-sdk';
+import { appLogger, paymentSDK } from '../payment-sdk';
 import { CreatePaymentRequest, MockPaymentServiceOptions } from './types/mock-payment.type';
 import { PaymentOutcome, PaymentResponseSchemaDTO } from '../dtos/mock-payment.dto';
 import { getCartIdFromContext, getPaymentInterfaceFromContext } from '../libs/fastify/context/context';
@@ -52,6 +52,7 @@ export class MockPaymentService extends AbstractPaymentService {
   public async status(): Promise<StatusResponse> {
     const handler = await statusHandler({
       timeout: getConfig().healthCheckTimeout,
+      log: appLogger,
       checks: [
         healthCheckCommercetoolsPermissions({
           requiredPermissions: [

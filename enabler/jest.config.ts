efbf337daebe09742e9da43134fd30afe3e8cc26
King-Hin Leung <kinghin.leung@commercetools.com>
2025-10-02 15:11:03 +0200

    test(enabler): fix the unit test config

--- enabler/jest.config.ts
+++ enabler/jest.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 
-module.exports = {
+export default {
     preset: 'ts-jest',
     testEnvironment: 'node',
     setupFiles: ['./test/jest.setup.ts'],
